1eec912ced15baa2af00f1b5357f679266eb2ab1
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-06-09 20:14:45 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
+import { useState } from "react";
 import "./App.css"
 import FormInput from "./components/FormInput";
 
 
 const App = () => {
+  const [username,setusername] = useState('');
+
+  console.log(username);
   return <div className="app">
     <form>
-      <FormInput placeholder="Username"/>
+      <FormInput placeholder="Username" setusername={setusername}/>
       <FormInput placeholder="Email"/>
       <FormInput placeholder="Full name"/>
       <FormInput placeholder="Age"/>
+      <button>Submit</button>
     </form>
 
   </div>;
