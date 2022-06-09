701a958e5b5e0ec4d02a37157fcb61db4d720591
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-06-09 19:35:47 +0530

    Updated

---
+++ src/components/FormInput.jsx
@@ -0,0 +1,12 @@
+import "./FormInput.css"
+
+const FormInput = () => {
+  return (
+    <div className="forminputs">
+        <label>UserName</label>
+        <input/>   
+    </div>
+  )
+}
+
+export default FormInput
