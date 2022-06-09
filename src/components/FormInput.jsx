a4dd4a985e3725ce213117731e78a0fa699388b2
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-06-09 20:37:33 +0530

    updated

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -4,7 +4,9 @@ const FormInput = (props) => {
   return (
     <div className="forminputs">
         {/* <label>UserName</label> */}
-        <input className="input" placeholder={props.placeholder} onChange={e=>props.setusername(e.target.value)}/> 
+        <input name={props.name}
+        
+        className="input" placeholder={props.placeholder}/> 
           
     </div>
   )
