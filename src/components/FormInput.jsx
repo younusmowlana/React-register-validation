2bb348de426d5425009c8b9f8989e4a14f134b7e
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-06-09 19:59:48 +0530

    updated

--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -4,7 +4,8 @@ const FormInput = (props) => {
   return (
     <div className="forminputs">
         {/* <label>UserName</label> */}
-        <input className="input" placeholder={props.placeholder}/>   
+        <input className="input" placeholder={props.placeholder}/> 
+          
     </div>
   )
 }
