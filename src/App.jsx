8352505c9e253083dcac382e41be9c15b51b0536
Younus Mowlana <83327699+younusmowlana@users.noreply.github.com>
2022-06-14 23:01:35 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,91 @@
-// import { useState } from "react";
-import "./App.css"
+import { useState } from "react";
+import "./App.css";
 import FormInput from "./components/FormInput";
 
-
 const App = () => {
-  // const [username,setusername] = useState('');
+  const [values, setValues] = useState({
+    username: "",
+    email: "",
+    birthday: "",
+    password: "",
+    confirmPassword: "",
+  });
 
-  const handlesubmit = (e)=>{
+  const inputs = [
+    {
+      id: 1,
+      name: "username",
+      type: "text",
+      placeholder: "Username",
+      errorMessage:
+        "Username should be 3-16 characters and shouldn't include any special character!",
+      label: "Username",
+      pattern: "^[A-Za-z0-9]{3,16}$",
+      required: true,
+    },
+    {
+      id: 2,
+      name: "email",
+      type: "email",
+      placeholder: "Email",
+      errorMessage: "It should be a valid email address!",
+      label: "Email",
+      required: true,
+    },
+    {
+      id: 3,
+      name: "birthday",
+      type: "date",
+      placeholder: "Birthday",
+      label: "Birthday",
+    },
+    {
+      id: 4,
+      name: "password",
+      type: "password",
+      placeholder: "Password",
+      errorMessage:
+        "Password should be 8-20 characters and include at least 1 letter, 1 number and 1 special character!",
+      label: "Password",
+      pattern: `^(?=.*[0-9])(?=.*[a-zA-Z])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,20}$`,
+      required: true,
+    },
+    {
+      id: 5,
+      name: "confirmPassword",
+      type: "password",
+      placeholder: "Confirm Password",
+      errorMessage: "Passwords don't match!",
+      label: "Confirm Password",
+      pattern: values.password,
+      required: true,
+    },
+  ];
+
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const data = new FormData(e.target)
-    console.log(Object.fromEntries(data.entries()));
-  }
+  };
 
-  // console.log(username);
-  return <div className="app">
-    <form onSubmit={handlesubmit}>
-      <FormInput name="username" placeholder="Username" />
-      <FormInput name="email" placeholder="Email"/>
-      <FormInput name="fullname" placeholder="Full name"/>
-      <FormInput name="age" placeholder="Age"/>
-      <button>Submit</button>
-    </form>
+  const onChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
 
-  </div>;
+  return (
+    <div className="app">
+      <form onSubmit={handleSubmit}>
+        <h1>Register</h1>
+        {inputs.map((input) => (
+          <FormInput
+            key={input.id}
+            {...input}
+            value={values[input.name]}
+            onChange={onChange}
+          />
+        ))}
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default App;
--- src/components/FormInput.jsx
+++ src/components/FormInput.jsx
@@ -1,15 +1,29 @@
-import "./FormInput.css"
+import { useState } from "react";
+import "./FormInput.css";
 
 const FormInput = (props) => {
-  return (
-    <div className="forminputs">
-        {/* <label>UserName</label> */}
-        <input name={props.name}
-        
-        className="input" placeholder={props.placeholder}/> 
-          
-    </div>
-  )
-}
+  const [focused, setFocused] = useState(false);
+  const { label, errorMessage, onChange, id, ...inputProps } = props;
 
-export default FormInput
+  const handleFocus = (e) => {
+    setFocused(true);
+  };
+
+  return (
+    <div className="formInput">
+      <label>{label}</label>
+      <input
+        {...inputProps}
+        onChange={onChange}
+        onBlur={handleFocus}
+        onFocus={() =>
+          inputProps.name === "confirmPassword" && setFocused(true)
+        }
+        focused={focused.toString()}
+      />
+      <span>{errorMessage}</span>
+    </div>
+  );
+};
+
+export default FormInput;
